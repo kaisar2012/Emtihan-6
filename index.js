0ee2bcc891cb2c153f912b558b86b347ab69b87f
Kaisar <kaisar.bolatkan@gmail.com>
2024-12-09 19:52:03 +0500

    aralyk baqylau

---
+++ index.js
@@ -0,0 +1,59 @@
+// Tapsirma 1
+
+// let school 
+// school = [
+// {
+//     name:"Example",
+//     phoneNumber:[+77119111123],
+//     students:{
+//         name:"Aidana",
+//         top:[24/99],
+//         progress:[1],
+//         lessons:[2]
+//     },{
+//         name:"Alidar",
+//         top:[19/88],
+//         progress:[87],
+//         lessons:[3]
+//     },{
+//         name:"Dias(Pias)",
+//         top:[25/87],
+//         progress:[32],
+//         lessons:[5]
+//     },{
+//         name:"Lasse",
+//         top:[22/32],
+//         progress:[68],
+//         lessons:[1]
+//     },{
+//         name:"Yeldos",
+//         top:[23/45],
+//         progress:[99],
+//         lessons:[4]
+//     }
+
+// }
+// ]
+
+// Tapsirma 2
+
+// for(so ){
+//     function name(changeSchoolName) {
+//         let name = ("Mektep atyn ozgertudu suraymyn!")
+//     }
+// }
+
+// Tapsirma 3
+
+
+// let number = prompt("Telephoen nomir ozgertu.10000 TENGE GE KANSHA KEREK SONZHA NOMER.AGAY POZHALUSTA HOTYABY 10 BALL BERINISHI.")
+
+// let()
+//         function name(changephoneNumber) {
+//             let name = ("Mektep atyn ozgertudu suraymyn!")
+//         }
+
+// Tapsirma 4
+
+
+    
